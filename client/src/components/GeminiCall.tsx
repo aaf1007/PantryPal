0d871da073533a92e9baa0112b27f15c36e530ec
Anton Florendo <antonflorendo7@gmail.com>
2026-03-07 00:08:11 -0800

    feat: implemented inital backend call to gemini

---
+++ client/src/components/GeminiCall.tsx
@@ -0,0 +1,31 @@
+import { useState } from "react";
+
+export default function GeminiCall() {
+  const [recipe, setRecipe] = useState("");
+  const [call, setCall] = useState(false)
+
+  async function generateRecipe() {
+    const res = await fetch("/api/recipes/generate", {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({ ingredients: ["rice", "eggs", "soy sauce"] }),
+    });
+
+    const text = await res.text();
+    setRecipe(text);
+  }
+
+  return (
+    <div>
+      <h1>Call Gemini</h1>
+      <button onClick={ async () => {
+        setCall(true);
+        await generateRecipe();
+        setCall(false);
+      }}>
+        {call ? `Waiting...` : `Call`}
+        </button>
+      {recipe && <div>{recipe}</div>}
+    </div>
+  );
+}
